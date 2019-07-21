4eb8d8d592e1a9cffa80732a683bd97fe634b0f5
totu <topituulensuu@fastmail.com>
2019-07-21 19:41:16 +0300

    week3

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
-import json
+
+WORKOUT_FILES = ["week1.txt", "week2.txt", "week3.txt"]
 
 def get_focus(day):
     day = int(day.split(" ")[1].split(":")[0])
@@ -16,47 +17,56 @@ def get_focus(day):
     else:
         return "Nothing..."
 
-workouts = []
-with open("week2.txt", "r") as file:
-    workout = []
-    day = None
-    for line in file.readlines():
-        if line.startswith("Week") or line.startswith("Exercise"):
-            continue
-        if line.startswith("Day"):
-            if day:
-                workouts.append(workout)
-                workout = []
-            day = line.rstrip()
-            continue
-        l = line.rstrip().split("\t")
-        l.append(day)
-        workout.append(l)
-    workouts.append(workout)
+def parse_workout(workout_file):
+    workouts = []
+    with open(workout_file, "r") as file:
+        workout = []
+        day = None
+        for line in file.readlines():
+            if line.startswith("Week") or line.startswith("Exercise"):
+                continue
+            if line.startswith("Day"):
+                if day:
+                    workouts.append(workout)
+                    workout = []
+                day = line.rstrip()
+                continue
+            l = line.rstrip().split("\t")
+            l.append(day)
+            workout.append(l)
+        workouts.append(workout)
 
-ex = []
+    ex = []
 
-for workout in workouts:
-    exercises = []
-    for exercise in workout:
-        name = exercise[0]
-        _set = exercise[1]
-        kg = exercise[2]
-        reps = exercise[3]
-        day = exercise[4]
-        focus = get_focus(day)
-        e = '{"name":"%s", "kg":"%s", "sets":"%s"}' % (name, kg, _set)
-        exercises.append(e)
-    title = '{"focus":"%s", "exercises":[%s]}' % (focus, ",".join(exercises))
-    ex.append(title)
+    for workout in workouts:
+        exercises = []
+        for exercise in workout:
+            name = exercise[0]
+            _set = exercise[1]
+            kg = exercise[2]
+            reps = exercise[3]
+            day = exercise[4]
+            focus = get_focus(day)
+            e = '{"name":"%s", "kg":"%s", "sets":"%s"}' % (name, kg, _set)
+            exercises.append(e)
+        title = '{"focus":"%s", "exercises":[%s], "reps":"%s"}' % (focus, ",".join(exercises), reps)
+        ex.append(title)
 
 
-x = '{"workouts": ['
-x += ",".join(ex)
-x += ']}'
+    x = '{"workouts": ['
+    x += ",".join(ex)
+    x += ']}'
 
-f = open("week1.json", "w")
-f.write(x)
-f.close()
-print(x)
+    json = workout_file.replace(".txt", ".json")
+    f = open(json, "w")
+    f.write(x)
+    f.close()
 
+def main():
+    for workout_file in WORKOUT_FILES:
+        print("parsing: %s" % workout_file)
+        parse_workout(workout_file)
+    print("done")
+
+if __name__ == "__main__":
+    main()
